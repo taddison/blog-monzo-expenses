ee447f4e5c89c216f8fddb676f92ef8630e3220a
Timothy Addison <git@tjaddison.com>
2018-04-22 12:39:37 +0100

    Update URLs, add support for HTTPS

--- Run.ps1
+++ Run.ps1
@@ -3,4 +3,5 @@
 $monzoExport = "c:\src\blog-monzo-expenses\Example\March2018Export.csv"
 $exportFolder = "c:\temp\expenses\March2018"
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Export-ExpenseFromMonzo -MonzoExport $monzoExport -ExportFolder $exportFolder
